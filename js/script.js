9a182ccdc6b1410d6c46da4a8cc6db6812b94280
AgataKondzior <ak.kondzior@gmail.com>
2017-06-13 11:43:52 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ function loadData() {
     $("body").append('<img class="bgimg" src="'+streetViewUrl+ '">');
  
     //  NYT AJAX request
-      var nytimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=fbcdeca7d9db4a279ce913f501a3647b'  
+      var nytimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=fbcdeca7d9db4a279ce913f501a3647b'  
         $.getJSON(nytimesURL, function(data){
             console.log(data);
             
