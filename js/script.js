4d1c145457b5054963c4f86ab60221a56c8b8544
AgataKondzior <ak.kondzior@gmail.com>
2017-06-16 11:07:34 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function loadData() {
     
     $greeting.text('So you want to live at ' + address + '?');
     
-    var streetViewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    var streetViewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="'+ streetViewUrl + ' ">');
  
     //  NYT AJAX request
